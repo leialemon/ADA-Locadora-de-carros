429e470e58cec5d2ff2549c3a6df4b9cd3762c77
Juliana Barros <julianabarrosf@protonmail.com>
2024-10-29 10:55:21 -0300

    criação do projeto spring boot

---
+++ locadora-de-carros/src/main/java/tech/ada/locadora_de_carros/LocadoraDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.locadora_de_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraDeCarrosApplication.class, args);
+	}
+
+}
---
+++ locadora-de-carros/src/test/java/tech/ada/locadora_de_carros/LocadoraDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.locadora_de_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
