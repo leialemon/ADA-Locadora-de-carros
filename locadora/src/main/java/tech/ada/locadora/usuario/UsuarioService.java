dfc6778ca0556d2c3f03561b5c55d8aaa16522bb
Juliana Barros <julianabarrosf@protonmail.com>
2024-11-01 20:16:42 -0300

    Criação de métodos de serviço de usuario

--- locadora/src/main/java/tech/ada/locadora/usuario/UsuarioService.java
+++ locadora/src/main/java/tech/ada/locadora/usuario/UsuarioService.java
@@ -1,8 +1,29 @@
 package tech.ada.locadora.usuario;
 
 import org.springframework.stereotype.Service;
+import tech.ada.locadora.exceptions.ObjetoNaoEncontradoException;
+
+import java.util.List;
+
 
 @Service
 public class UsuarioService {
-    
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void criarUsuario(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarUsuarioPorCpf(String cpf){
+        return usuarioRepository.findByCpf(cpf).orElseThrow(() -> new ObjetoNaoEncontradoException("Usuário não encontrado!"));
+    }
+
+    public List<Usuario> mostrarTodosOsUsuarios(){
+        return usuarioRepository.findAll();
+    }
 }
